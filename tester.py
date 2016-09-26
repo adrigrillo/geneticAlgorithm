0ebbbf0a958fb1710f093b2840ce554c29c07877
LUIS BUCETA OJEDA <a0303573@lab.inf.uc3m.es>
2016-09-26 18:59:43 +0200

    Tester funciona!

--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from generator import *
 import urllib2
+from decimal import *
 
 def getfitness(chromosome):
     petition = "http://163.117.164.230/age/?f=test&c=" + chromosome
@@ -14,7 +15,7 @@ def evaluator(poblacion):
     for i in range(0, len(poblacion)):
             fitness = getfitness(poblacion[i])
             print ("aux " + str(aux) + " fitness = " + str(fitness))
-            if (fitness < aux):
+            if (Decimal(fitness) < Decimal(aux)):
                     print ("WOLOLO " + str(aux) + " fHEHEHE = " + str(fitness))
                     aux = fitness
                     bina_aux = poblacion[i]
