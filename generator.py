be5423937278880dc591f31be32f60c06fd3dee6
Adrian Rodriguez Grillo <adrirgrillo@gmail.com>
2016-09-26 18:19:41 +0200

    generador creado

---
+++ generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import itertools, random
+
+def generarPoblacion(numIndividuos):
+    lst = list(itertools.product([0, 1], repeat=16))
+    generador = []
+    for i in range(0, numIndividuos):
+        generador.append(lst[random.randint(0, pow(2,16))] + lst[random.randint(0, pow(2,16))] + lst[random.randint(0, pow(2,16))] + lst[random.randint(0, pow(2,16))])
+    poblacion = []
+    for i in range(0, numIndividuos):
+        cromosoma = ""
+        for j in range(0, 64):
+            cromosoma += str(generador[i][j])
+        poblacion.append(cromosoma)
+    return poblacion
