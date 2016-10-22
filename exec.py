28aa93bdecfc3c6d80455076645d32d36de8f027
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2016-10-22 14:56:33 +0200

    Criterio de parada a prueba de ruido y con flag de activacion

--- exec.py
+++ exec.py
@@ -4,5 +4,6 @@ from main import *
 
 """ Esta clase será utilizada para hacer las correspondientes llamadas al algoritmo
     para ello se llamara a la funcion main de la siguiente forma:
-    main(tamanoPoblacion = (valor), numGeneraciones = (valor), tasaMutacion = (valor), partipantesTorneo = (valor))"""
-main(tamanoPoblacion = 10, numGeneraciones = 1, tasaMutacion = 100, partipantesTorneo = 4)
+    main(tamanoPoblacion = (valor), numGeneraciones = (valor), tasaMutacion = (valor), partipantesTorneo = (valor),
+         parada = (True/False))"""
+main(tamanoPoblacion=10, numGeneraciones=100, tasaMutacion=100, partipantesTorneo=4, parada=True)
