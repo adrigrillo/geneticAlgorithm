fb0c2b3be8a4eef7c25c4ca31d2fbf49f96579c3
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2016-10-22 13:03:53 +0200

    Actualizado el fichero de salida para ver parametros y cambio en la de parada para evitar ruido el lunes

--- exec.py
+++ exec.py
@@ -5,4 +5,4 @@ from main import *
 """ Esta clase será utilizada para hacer las correspondientes llamadas al algoritmo
     para ello se llamara a la funcion main de la siguiente forma:
     main(tamanoPoblacion = (valor), numGeneraciones = (valor), tasaMutacion = (valor), partipantesTorneo = (valor))"""
-main(tamanoPoblacion = 100, numGeneraciones = 200, tasaMutacion = 100, partipantesTorneo = 4)
+main(tamanoPoblacion = 100, numGeneraciones = 500, tasaMutacion = 100, partipantesTorneo = 4)
